8218b91e06ae0fe2780ee4d7124cba89e9001358
CristianmMst <cristianmmst@gmail.com>
2024-07-24 19:52:26 -0500

    add auth dtos

--- src/auth/application/authService.ts
+++ src/auth/application/authService.ts
@@ -1,5 +1,6 @@
 import { User } from "../../user/domain/user";
 import { TokenService } from "./tokenService";
+import { LoginUserDto, RegisterUserDto } from "./dtos/authDtos";
 import { UserRepository } from "../../user/domain/userRepository";
 import { comparePassword, hashPassword } from "../../shared/utils/hash";
 import { UserNotFoundError } from "../../user/domain/userNotFoundError";
@@ -10,13 +11,13 @@ export class AuthService {
     private readonly userRepository: UserRepository,
   ) {}
 
-  register = async (userData: User): Promise<void> => {
+  register = async (userData: RegisterUserDto): Promise<void> => {
     const hashedPassword = await hashPassword(userData.password);
     const user = new User(userData.email, userData.username, hashedPassword);
     await this.userRepository.save(user);
   };
 
-  login = async ({ email, password }: User): Promise<string> => {
+  login = async ({ email, password }: LoginUserDto): Promise<string> => {
     const user = await this.userRepository.findByEmail(email);
     if (!user) throw new UserNotFoundError("User not found");
 
