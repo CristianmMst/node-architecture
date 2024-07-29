7e38dbaa283d5ef18a6466d3ceb4b242ea9447f8
CristianmMst <cristianmmst@gmail.com>
2024-07-28 23:40:03 -0500

    add UserDto and modify useCase

--- src/user/application/userFindById.ts
+++ src/user/application/userFindById.ts
@@ -1,13 +1,16 @@
-import { User } from "../domain/user";
+import { UserDto } from "./dtos/userDto";
 import { UserRepository } from "../domain/userRepository";
 import { UserNotFoundError } from "../domain/exceptions/userNotFoundError";
 
 export class UserFindById {
   constructor(protected readonly userRepository: UserRepository) {}
 
-  async run(id: string): Promise<User> {
+  async run(id: string): Promise<UserDto> {
     const user = await this.userRepository.findById(id);
     if (!user) throw new UserNotFoundError();
-    return user;
+
+    const { password: _, ...userWithoutPassword } = user;
+
+    return userWithoutPassword;
   }
 }
